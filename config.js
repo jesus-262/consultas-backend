bda471413dd1144b12cbb887ca533ef8898a127e
jesus-262 <jesus-262@hotmail.com>
2022-11-29 22:16:40 -0500

    db3

--- config.js
+++ config.js
@@ -1,9 +1,9 @@
 const config = {}
-config.db_host=process.env.HOST || 'localhost';
-config.db_user=process.env.USER || 'root';
-config.db_password=process.env.PASSWORD || '';
-config.db_database=process.env.DATABASE || 'datos';
-config.db_port=process.env.PORT || 3306;
+config.db_host=process.env.DB_HOST || 'localhost';
+config.db_user=process.env.DB_USER || 'root';
+config.db_password=process.env.DB_PASSWORD || '';
+config.db_database=process.env.DB_DATABASE || 'datos';
+config.db_port=process.env.DB_PORT || 3306;
 
 
 module.exports = config;
