89891515bd13516a277b04f33c6e850eff996750
jesus-262 <jesus-262@hotmail.com>
2022-11-30 15:45:59 -0500

    borrar codigo que no sirve

--- controllers/consulta.controllers.js
+++ controllers/consulta.controllers.js
@@ -23,27 +23,8 @@ puppeteer.use(
 );
 
 //mostrar varios
-consultacontrol.postConsulta = async(req, res)=>{
-  
- //poli('66946183'); 
- regis('66946183');
 
-  res.send("termino");
-  //await preparePageForTests(page);
-    console.log("entro")
-}
-consultacontrol.getConsulta = async(req, res)=>{
-  
- 
- 
-  poli('66946183'); 
-  //66946183
-  //regis('1107071154');
- 
-   res.send("termino");
-   //await preparePageForTests(page);
-     console.log("entro")
- }
+
  /*
  (async () => {
   
@@ -70,139 +51,12 @@ consultacontrol.getConsulta = async(req, res)=>{
   await browser.close();
 })();
 */
- consultacontrol.getConsultaNombre = async(req, res)=>{
- /* const stats = PCR.getStats();
-  if (stats) {
-    const browser= await   stats.puppeteer.launch({
-          headless: false,
-          args: ["--no-sandbox"],
-          executablePath: stats.executablePath
-      }).then(function(browser){
-          //...
-      }).catch(function(error) {
-          console.log(error);
-      });
-   }*/
-   /*
-   execFile(chromium.path, ['https://google.com'], err => {
-	console.log('Hello Google!');
-});*/
-   const browser= await puppeteer.launch({headless:true, executablePath: chromium.path});
-  //'/usr/bin/chromium-browser'
-  // const browser= await puper.launch({headless:false, executablePath:  'C:/Program Files/Google/Chrome/Application/chrome.exe'});
-   let page= await browser.newPage();
-   console.log("BUSCANDO NOMBRE")
-   
-   console.log("10%")
-   const html1='https://antecedentes.policia.gov.co:7005/WebJudicial/antecedentes.xhtml';
-   const html2='https://www.procuraduria.gov.co/Pages/Consulta-de-Antecedentes.aspx';
-   const html3='https://www.skynovels.net/';
-   await page.setDefaultNavigationTimeout(0); 
-   await page.goto(html1,{waitUntil: 'networkidle2'}).catch(e => {
-    console.log('FAIL');
-    res.send("caida");
-  });;
-   //await page.type('.devsite-search-field', 'Headless Chrome');
  
-   console.log("20%")
- 
-   await page.waitForTimeout(2000)
- 
-  
-   //no#main-wrapper > app-home > article > section.container.container-home > div:nth-child(1) > h2
-   await page.evaluate(async() => {});
-  
-     await page.waitForSelector(`#aceptaOption > tbody > tr > td:nth-child(1) > input[type="radio"]`).catch(e => {
-      console.log('FAIL #aceptaOption > tbody > tr > td:nth-child(1) > input[type="radio"]');
-      res.send("XXXXXX");
-    }); 
-       // Hace que .then() devuelva una promesa rechazada
-       console.log( '30%' );
-       let elementt = await page.$('#aceptaOption > tbody > tr > td:nth-child(1) > input[type="radio"]')
-       elementt.click();
-       await page.waitForTimeout(2000)
-       console.log( '40%' );
-       await page.waitForSelector('#continuarBtn').catch(e => {
-        console.log('FAIL #continuarBtn"');
-        res.send("XXXXXX");
-      }); 
-       
-         console.log( '50%' );
-      
-         const form = await page.$('#continuarBtn');
-        
-         form.click();
-         await page.waitForTimeout(4000)
-         await page.waitForSelector(`#cedulaInput`).then(async() => {
-  // Hace que .then() devuelva una promesa rechazada
-          
-         
-  let element = await page.$('#cedulaInput');
-  // element.i="1107071154"
-   //element.innerText="1107071154";
-
- 
-  
-await page.evaluate((el,cedu) =>{
-el.value=cedu 
-
-
-}, element,cedula);
-//await page.evaluate(el => el.value="dd", element);
-
-
-console.log( '60%' );
-await page.waitForTimeout(1000)
-
-//j_idt17
-
-const {solved, error}= await page.solveRecaptchas();
-if(solved){
-
-  const form = await page.$('#j_idt17');
-
-form.click();
-console.log( '80%' );
-await page.waitForTimeout(2000)
-
-
-var data = await page.$$eval('table tr td span b', tds => tds.map((b) => {
- return b.innerHTML;
-}));
-
-//console.log(data);
-//data.length 6 si tiene cedula
-//data.length 4 si no tiene cedula
-await page.close();
-await browser.close();
-if(data.length==4){
- console.log( '100%' );
-// return console.log("NO EXISTE LA CEDULA")
-res.send ("NO EXISTE LA CEDULA");
-}else{
- console.log( '100%' );
- console.log(data[2]);
- res.send (data[2]);
-
-}
-
-
-
-}
-
-
-         }).catch(e => {
-          console.log('FAIL');
-          res.send("XXXXXX");
-        });
-         
-    
- }
  consultacontrol.postConsultaNombre = async(req, res)=>{
-  console.log("envio postConsultaNombre")
+  console.log("prueba")
   const {cedula}=req.body;
   console.log(cedula);
-  
+  //const browser= await puppeteer.launch({headless:false, executablePath: chromium.path});
   const browser= await puppeteer.launch({headless:true, executablePath: executablePath()});
   // const browser= await puper.launch({headless:false, executablePath:  'C:/Program Files/Google/Chrome/Application/chrome.exe'});
    let page= await browser.newPage();
--- routes/consulta.js
+++ routes/consulta.js
@@ -1,13 +1,13 @@
 const {Router} = require('express');
 const router = Router();
 
-const {postConsulta,getConsulta, postConsultaNombre,postConsultaLugarDeVotacion,getConsultaNombre} = require('../controllers/consulta.controllers');
+const { postConsultaNombre,postConsultaLugarDeVotacion,getConsultaNombre} = require('../controllers/consulta.controllers');
 
-router.post('/', postConsulta);
-router.get('/', getConsulta);
+
+
 
 router.post('/consulta/nombre', postConsultaNombre);
-router.get('/consulta/nombre/:cedula', getConsultaNombre);
+
 router.post('/consulta/lugar', postConsultaLugarDeVotacion);
 
 
