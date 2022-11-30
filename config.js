9baeceaad6816bfbccf2d04bcbefd331dd510344
jesus-262 <jesus-262@hotmail.com>
2022-11-29 23:25:32 -0500

    bd5

--- config.js
+++ config.js
@@ -3,7 +3,7 @@ config.db_host=process.env.DB_HOST || 'localhost';
 config.db_user=process.env.DB_USER || 'root';
 config.db_password=process.env.DB_PASSWORD || '';
 config.db_database=process.env.DB_DATABASE || 'datos';
-config.port=process.env.DB_PORT || 4000;
+config.port=process.env.PORT || 4000;
 
 
 config.db_port=process.env.DB_PORT || 3306;
