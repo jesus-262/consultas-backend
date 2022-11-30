a3b476dc6e819df6db1132c4dc37959d28861ff3
jesus-262 <jesus-262@hotmail.com>
2022-11-29 23:46:26 -0500

    mysql2

--- database.js
+++ database.js
@@ -1,8 +1,29 @@
-const mysql =require('mysql');
+//var db =require('mysql2/promise');
 const { database } = require ('./keys');
-const {promisify} =require('util')
+//const {promisify} =require('util')
+const mysql = require('mysql2');
 
-const pool = mysql.createPool(database);
+var pool = mysql.createPool(database);
+pool.getConnection(function(err, connection) {
+    // Do something with the connection
+    if(err){
+        console.log("error");
+     }
+     if(connection){
+        // connection.release();
+         pool.releaseConnection(connection);
+         console.log("base de datos funcionando");
+         return;
+     }
+ //   conn.query(/* ... */);
+    // Don't forget to release the connection when finished!
+   // pool.releaseConnection(conn);
+ })
+
+/*
+const pool = createPool(database).then(() => {
+    console.log("base de datos funcionando");
+}); 
 
 
 pool.getConnection((err,connection)=>{
@@ -15,9 +36,9 @@ if(connection){
     console.log("base de datos funcionando");
     return;
 }
-});
+});*/
 //para poder utilizar promesas
-pool.query = promisify(pool.query);
+//pool.query = promisify(pool.query);
 
 
 module.exports = pool;
