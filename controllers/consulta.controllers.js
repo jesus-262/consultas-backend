3830e5c7ed35c6480613ffdeb3b6d1701fbdc145
jacopete <jaccopete@gmail.com>
2023-06-07 11:16:10 -0500

    cedula repetida

--- controllers/consulta.controllers.js
+++ controllers/consulta.controllers.js
@@ -592,8 +592,9 @@ consultacontrol.postConsultaNombre = async(req, res)=>{
   await page.setDefaultNavigationTimeout(0); 
   page.setUserAgent('Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/61.0.3163.100 Safari/537.36');
   await page.goto('https://wsp.registraduria.gov.co/censo/consultar');
+  //....
 
-   console.log("10%")
+   console.log("10 %")
   await page.waitForTimeout(4000)
   await page.screenshot({
     path: 'example.png'
